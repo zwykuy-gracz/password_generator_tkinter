3ec19cdfd4ee7092bde080e194b435f4f7e2b0c7
Paul <you@example.com>
2022-10-01 17:47:17 +0200

    ready to go

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import string
 import random
 import pyperclip
+import json
 
 letters = string.ascii_letters
 digits = string.digits
@@ -21,19 +22,43 @@ def save_to_file():
     website_url = website_entry.get()
     username = email_entry.get()
     password = generated_passwrod.get()
+    new_data = {website_url.lower(): {
+        'email': username,
+        'password': password,
+    }}
 
     if len(website_url) == 0 or len(password) == 0:
         messagebox.showinfo(title='Oops', message='Fill empty fields')
     else:
-        is_ok = messagebox.askokcancel(title=website_url, message='Correct data?')
-        
-        if is_ok:
-            with open("data.txt", 'a') as file:
-                file.write(f"{website_url} | {username} | {password}\n")
-
+        try:
+            with open("data.json", 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
             website_entry.delete(0, END)
             generated_passwrod.delete(0, END)
 
+def search_website():
+    website_url = website_entry.get()
+    website = website_url.lower()
+    try:
+        with open("data.json", 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message="No data file found")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website.title(), message=f"email: {email}\npassword: {password}")
+        else:
+            messagebox.showinfo(title='Error', message=f"No details for {website} exists.")
 
 window = Tk()
 window.title("Password Manager")
@@ -47,9 +72,11 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text='Website:')
 website_label.grid(column=0, row=1)
-website_entry = Entry(width=45)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=30)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
+website_button = Button(text='Search', width=15, command=search_website)
+website_button.grid(column=2, row=1)
 
 email_label = Label(text='Email/Username:')
 email_label.grid(column=0, row=2)
